0c2c7702dd55d1bbc6318e0b61643b3e02a1e593
Ingo_Igel <224564910+Ingo-Igel@users.noreply.github.com>
2025-08-09 18:35:26 +0200

    first commit

---
+++ data/base/graphics/entity/cargo-wagon/cargo-wagon-mask.lua
@@ -0,0 +1,14 @@
+return
+{
+  width = 400*2,
+  height = 372*2,
+  shift = util.by_pixel( 1.0, -30.0),
+  line_length = 4,
+  filenames = {
+    "-1.png",
+    "-2.png",
+    "-3.png",
+    "-4.png",
+  },
+  lines_per_file = 8,
+}
---
+++ data/base/graphics/entity/cargo-wagon/cargo-wagon.lua
@@ -0,0 +1,14 @@
+return
+{
+  width = 442*2,
+  height = 408*2,
+  shift = util.by_pixel( 0.0, -25.0),
+  line_length = 4,
+  filenames = {
+    "-1.png",
+    "-2.png",
+    "-3.png",
+    "-4.png",
+  },
+  lines_per_file = 8,
+}
